b1384c6e63dafa4c1100b98c3a5807f5230bc439
naftali <shmerler3@gmail.com>
2021-03-23 17:03:15 +0200

    first commit

---
+++ src/app/services/bluetooth.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { BluetoothSerial } from '@ionic-native/bluetooth-serial/ngx';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class BluetoothService {
+  constructor(private bluetoothSerial: BluetoothSerial) {
+    // Write a string
+    this.bluetoothSerial.write('hello world').then((success) => {
+      console.log('success', success);
+    });
+
+    // Array of int or bytes
+    this.bluetoothSerial.write([186, 220, 222]).then((success) => {
+      console.log('success', success);
+    });
+
+    this.bluetoothSerial.read().then((data)=>{
+      console.log("data",data);
+      
+    });
+  
+  }
+}
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BluetoothService } from '../services/bluetooth.service';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +8,10 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(public BluetoothService : BluetoothService) {}
+
+  getData(){
+    alert("getData")
+  }
 
 }
